bc931e48b23b3fa9fb3b6e4a254e42dcc78b58d8
Divilioglu <mo.divilioglu@gmail.com>
2021-06-02 09:57:58 +0100

    [DTD-359] - Added log to verify logging feature

--- app/uk/gov/hmrc/timetopayproxy/controllers/MicroserviceHelloWorldController.scala
+++ app/uk/gov/hmrc/timetopayproxy/controllers/MicroserviceHelloWorldController.scala
@@ -19,13 +19,16 @@ package uk.gov.hmrc.timetopayproxy.controllers
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 import play.api.mvc.{Action, AnyContent, ControllerComponents}
 import javax.inject.{Inject, Singleton}
+import org.apache.commons.logging.LogFactory
+
 import scala.concurrent.Future
 
 @Singleton()
 class MicroserviceHelloWorldController @Inject()(cc: ControllerComponents)
     extends BackendController(cc) {
-
+  private val logger = LogFactory.getLog(classOf[MicroserviceHelloWorldController])
   def hello(): Action[AnyContent] = Action.async { implicit request =>
+    logger.info("Hello world by time-to-pay-proxy!")
     Future.successful(Ok("Hello world"))
   }
 }
