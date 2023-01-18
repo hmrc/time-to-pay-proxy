fa89ead7404ddbb4a0a81544ba9c46188db50080
koladeadewuyi <koladeadewuyi@users.noreply.github.com>
2023-01-18 11:41:03 +0000

    DTD-1584 Add correlation Id in Kibana logs (#92)

--- app/uk/gov/hmrc/timetopayproxy/logging/RequestAwareLogger.scala
+++ app/uk/gov/hmrc/timetopayproxy/logging/RequestAwareLogger.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 HM Revenue & Customs
+ * Copyright 2023 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,24 +19,28 @@ package uk.gov.hmrc.timetopayproxy.logging
 import org.slf4j.MDC
 import play.api.Logger
 import uk.gov.hmrc.http.{ HeaderCarrier, RequestId }
+import uk.gov.hmrc.timetopayproxy.models.HeaderNames.{ CorrelationIdKey, RequestIdKey }
 
 class RequestAwareLogger(clazz: Class[_]) {
 
   private val underlying = Logger(clazz)
 
-  val requestIdKey = "x-request-id"
+  def trace(msg: => String)(implicit hc: HeaderCarrier): Unit = withRequestIDsInMDC(underlying.trace(msg))
+  def debug(msg: => String)(implicit hc: HeaderCarrier): Unit = withRequestIDsInMDC(underlying.debug(msg))
+  def info(msg: => String)(implicit hc: HeaderCarrier): Unit = withRequestIDsInMDC(underlying.info(msg))
+  def warn(msg: => String)(implicit hc: HeaderCarrier): Unit = withRequestIDsInMDC(underlying.warn(msg))
+  def error(msg: => String)(implicit hc: HeaderCarrier): Unit = withRequestIDsInMDC(underlying.error(msg))
 
-  def trace(msg: => String)(implicit hc: HeaderCarrier): Unit = withRequestIDInMDC(underlying.trace(msg))
-  def debug(msg: => String)(implicit hc: HeaderCarrier): Unit = withRequestIDInMDC(underlying.debug(msg))
-  def info(msg: => String)(implicit hc: HeaderCarrier): Unit = withRequestIDInMDC(underlying.info(msg))
-  def warn(msg: => String)(implicit hc: HeaderCarrier): Unit = withRequestIDInMDC(underlying.warn(msg))
-  def error(msg: => String)(implicit hc: HeaderCarrier): Unit = withRequestIDInMDC(underlying.error(msg))
-
-  def withRequestIDInMDC(f: => Unit)(implicit hc: HeaderCarrier): Unit = {
+  private def withRequestIDsInMDC(f: => Unit)(implicit hc: HeaderCarrier): Unit = {
     val requestId = hc.requestId.getOrElse(RequestId("Undefined"))
-    MDC.put(requestIdKey, requestId.value)
+    val correlationId = hc.otherHeaders
+      .collectFirst { case (key, value) if key.equalsIgnoreCase(CorrelationIdKey) => value }
+
+    MDC.put(RequestIdKey, requestId.value)
+    correlationId.foreach(MDC.put(CorrelationIdKey, _))
     f
-    MDC.remove(requestIdKey)
+    MDC.remove(RequestIdKey)
+    correlationId.foreach(_ => MDC.remove(CorrelationIdKey))
   }
 
 }
