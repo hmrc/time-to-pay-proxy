b1ccc0f3c26893094e57086482480ac544a5e48e
Alex Olkhovskiy <lxol@users.noreply.github.com>
2025-09-09 14:34:29 +0100

    DTD-2858: Add TtpFeedbackLoopService to handle TTP cancellation logic

---
+++ app/uk/gov/hmrc/timetopayproxy/services/TtpFeedbackLoopService.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.timetopayproxy.services
+
+import com.google.inject.ImplementedBy
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.timetopayproxy.connectors.TtpFeedbackLoopConnector
+import uk.gov.hmrc.timetopayproxy.models.error.TtppEnvelope.TtppEnvelope
+import uk.gov.hmrc.timetopayproxy.models.saopled.ttpcancel.{ TtpCancelRequest, TtpCancelInformativeResponse }
+
+import javax.inject.{ Inject, Singleton }
+import scala.concurrent.ExecutionContext
+
+@ImplementedBy(classOf[DefaultTtpFeedbackLoopService])
+trait TtpFeedbackLoopService {
+  def cancelTtp(
+    ttppRequest: TtpCancelRequest
+  )(implicit ec: ExecutionContext, hc: HeaderCarrier): TtppEnvelope[TtpCancelInformativeResponse]
+}
+
+@Singleton
+class DefaultTtpFeedbackLoopService @Inject() (ttppConnector: TtpFeedbackLoopConnector) extends TtpFeedbackLoopService {
+  def cancelTtp(
+    ttppRequest: TtpCancelRequest
+  )(implicit ec: ExecutionContext, hc: HeaderCarrier): TtppEnvelope[TtpCancelInformativeResponse] =
+    ttppConnector.cancelTtp(ttppRequest)
+}
