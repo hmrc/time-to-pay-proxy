2a5a275d74bb3a6771a608548a160a7a5eda5e3b
Aashvin <55795472+Aashvin@users.noreply.github.com>
2025-09-10 16:07:55 +0100

    Adds missing formats

--- app/uk/gov/hmrc/timetopayproxy/models/saopled/ttpinform/TtpInformRequest.scala
+++ app/uk/gov/hmrc/timetopayproxy/models/saopled/ttpinform/TtpInformRequest.scala
@@ -1,8 +1,10 @@
 package uk.gov.hmrc.timetopayproxy.models.saopled.ttpinform
 
 import cats.data.NonEmptyList
+import play.api.libs.json.{Format, Json, OFormat}
 import uk.gov.hmrc.timetopayproxy.models.{ChannelIdentifier, Identification}
 import uk.gov.hmrc.timetopayproxy.models.saopled.common.{SaOpLedInstalment, TransitionedIndicator}
+import uk.gov.hmrc.timetopayproxy.utils.json.CatsNonEmptyListJson
 
 final case class TtpInformRequest(
   identifications: NonEmptyList[Identification],
@@ -11,3 +13,11 @@ final case class TtpInformRequest(
   channelIdentifier: ChannelIdentifier,
   transitioned: Option[TransitionedIndicator]
 )
+
+object TtpInformRequest {
+  implicit val format: OFormat[TtpInformRequest] = {
+    implicit def nelFormat[T: Format]: Format[NonEmptyList[T]] = CatsNonEmptyListJson.nonEmptyListFormat
+
+    Json.format[TtpInformRequest]
+  }
+}
