b95c041c8e44e09fac3605a6557ef157f0dc14b2
sainahjan <141128938+sainahjan@users.noreply.github.com>
2025-10-03 14:37:57 +0100

    DTD-3924: Update minor version of scalamock.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -16,7 +16,7 @@ object AppDependencies {
   val test = Seq(
     "uk.gov.hmrc"                      %% "bootstrap-test-play-30"      % BootstrapPlayVersion            % Test,
     "org.scalatest"                    %% "scalatest"                   % "3.2.19"                         % Test,
-    "org.scalamock"                    %% "scalamock"                   % "7.3.2"                         % Test,
+    "org.scalamock"                    %% "scalamock"                   % "7.5.0"                         % Test,
     "org.playframework"                %% "play-test"                   % PlayVersion.current             % Test,
     "com.fasterxml.jackson.module"     %% "jackson-module-scala"        % "2.19.0"                        % Test,
     "com.vladsch.flexmark"             %  "flexmark-all"                % "0.64.8"                        % Test,
