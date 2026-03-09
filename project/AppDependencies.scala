3c8d3a625ae5719eebc3016c059abf874190e53c
vinniebrice <vinniebrice2@gmail.com>
2026-03-09 11:52:40 +0000

    DTD-NOJIRA/Update-HMRC-Play: Upgrade library version to remove vulnerability

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val BootstrapPlayVersion = "10.4.0"
+  private val BootstrapPlayVersion = "10.7.0"
 
   val compile = Seq(
     // This is necessary until the HMRC/Play dependencies bring in the version of Jackson that is not insecure.
