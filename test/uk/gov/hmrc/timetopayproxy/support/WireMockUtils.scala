6e386646bdcf323bfdd8cc9b523ae11246706a76
Graeme Cross <240507183+graemecross1@users.noreply.github.com>
2026-06-24 15:54:03 +0100

    DTD-4393: Moved shared resistry clear from before all to main body of trait to fix some instances of tests not running in intellij

--- test/uk/gov/hmrc/timetopayproxy/support/WireMockUtils.scala
+++ test/uk/gov/hmrc/timetopayproxy/support/WireMockUtils.scala
@@ -40,6 +40,8 @@ trait WireMockUtils extends BeforeAndAfterEach with BeforeAndAfterAll with Guice
 
   val wireMockServer: WireMockServer = new WireMockServer(wireMockConfig().port(wireMockPort))
 
+  SharedMetricRegistries.clear()
+
   override implicit lazy val app: Application = GuiceApplicationBuilder()
     .configure("auditing.enabled" -> "false")
     .build()
@@ -47,7 +49,6 @@ trait WireMockUtils extends BeforeAndAfterEach with BeforeAndAfterAll with Guice
   override def beforeAll(): Unit = {
     super.beforeAll()
     wireMockServer.start()
-    SharedMetricRegistries.clear()
     WireMock.configureFor("localhost", wireMockPort)
   }
 
