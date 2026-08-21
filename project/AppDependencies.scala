40bd9b4127b5609992b408d5d0d148c8026e5a8b
Muhammad Imran <224537224+mimran-HMRC@users.noreply.github.com>
2026-08-21 18:07:16 +0100

    NO-JIRA: Dependency mob updates (#201)
    * Update dependencies
    
    * Revert jackson libraries to v2.20.2

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -22,14 +22,14 @@ object AppDependencies {
     "com.vladsch.flexmark"          % "flexmark-all"           % "0.64.8"             % Test,
     "org.scalatestplus.play"       %% "scalatestplus-play"     % "7.0.2"              % Test,
     "com.networknt"                 % "json-schema-validator"  % "2.0.4"              % Test,
-    "com.softwaremill.quicklens"   %% "quicklens"              % "1.9.12"             % Test
+    "com.softwaremill.quicklens"   %% "quicklens"              % "1.9.15"             % Test
   )
 
   val dependencyOverrides = Seq(
     "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.20.2",
     "com.fasterxml.jackson.core"    % "jackson-databind"     % "2.20.2",
     "uk.gov.hmrc"                  %% "http-verbs-play-30"   % "15.8.0",
-    "org.slf4j"                     % "slf4j-api"            % "2.0.17",
-    "com.google.guava"              % "guava"                % "32.1.3-jre"
+    "org.slf4j"                     % "slf4j-api"            % "2.0.18",
+    "com.google.guava"              % "guava"                % "33.7.1-jre"
   )
 }
